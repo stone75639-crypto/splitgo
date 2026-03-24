e65f1a3d24553567c49c9a72e86f72663f406f72
stone75639-crypto <stone75639@smail.nchu.edu.tw>
2026-03-24 17:10:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ★ 每次更新 index.html 後，把版本號 +1，iOS 就會自動清除舊快取
-const CACHE_NAME = 'splitgo-v12';
+const CACHE_NAME = 'splitgo-v13';
 
 const STATIC_ASSETS = [
   'https://cdn.jsdelivr.net/npm/chart.js',
